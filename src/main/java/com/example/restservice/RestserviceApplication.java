0b04001114505a22aa262e696b4f92a813b859e1
kkugler <kaikugler@gmx.de>
2018-10-25 11:53:07 +0200

    Mehr Überflüssiges entfernt

--- src/main/java/com/example/restservice/RestserviceApplication.java
+++ src/main/java/com/example/restservice/RestserviceApplication.java
@@ -3,11 +3,9 @@ package com.example.restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@RestController
 public class RestserviceApplication {
 
     public static void main(String[] args) {
